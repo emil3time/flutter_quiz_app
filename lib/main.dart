debd76536f72067fbbbd304a9eecc0f7fd196d0a
Emil <ezmil34@gmail.com>
2021-12-22 19:01:25 +0100

    add elevatedButtons and basic methods

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,37 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var index = 0;
+  void chooseAnswer() {
+    index = index + 1;
+    print(index);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your faworite song?',
+      'Do you like ice creams?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('This is AppBar Wigget!'),
         ),
         body: Column(
-          children: <Widget>[
-            // Text(questions.elementAt(questionIndex)),
+          children: [
+            Text(questions[0]),
+            ElevatedButton(onPressed: chooseAnswer, child: Text(' Answer 1')),
+            ElevatedButton(
+                onPressed: () => print('Answr 2'), child: Text(' Answer 1')),
+            ElevatedButton(
+                onPressed: () {
+                  // sdjhfuydsg
+                  ///skdashy//
+                  print('Answr3');
+                },
+                child: Text(' Answer 1')),
           ],
         ),
       ),
